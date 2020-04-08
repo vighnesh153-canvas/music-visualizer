93eddeaa77198e02677dcb9e1cb686f8af9bd70a
vighnesh153 <vighnesh.raut13@gmail.com>
2020-04-08 17:04:34 +0530

    fix the audio context called before user gestures bug

--- ts/index.ts
+++ ts/index.ts
@@ -6,6 +6,8 @@ import { ICanvasAnimation } from "./IAnimation";
 
     let analyzer: AnalyserNode;
     let animationRunning = true;
+    let isInit = true;
+    let audioElement: HTMLAudioElement;
     const byteFrequencyDataArray = new Uint8Array(1024);
     const byteTimeDomainDataArray = new Uint8Array(1024);
 
@@ -19,6 +21,18 @@ import { ICanvasAnimation } from "./IAnimation";
     };
 
     const animateAll = () => {
+        if (isInit) {
+            const audioContext = new AudioContext();
+            analyzer = audioContext.createAnalyser();
+
+            const audioSource = audioContext.createMediaElementSource(audioElement);
+            audioSource.connect(analyzer);
+            analyzer.connect(audioContext.destination);
+
+
+            isInit = false;
+        }
+
         animationRunning = true;
         analyzer.fftSize = 2048;
         analyzer.smoothingTimeConstant = 0.9;
@@ -37,14 +51,7 @@ import { ICanvasAnimation } from "./IAnimation";
     window.onload = () => {
         console.log('App bootstrapping!');
 
-        const audioContext = new AudioContext();
-        analyzer = audioContext.createAnalyser();
-
-        const audioElement = document.querySelector('audio');
-        const audioSource = audioContext.createMediaElementSource(audioElement);
-        audioSource.connect(analyzer);
-        analyzer.connect(audioContext.destination);
-
+        audioElement = document.querySelector('audio');
         audioElement.addEventListener('play', animateAll);
         audioElement.addEventListener('pause', stopAllAnimations);
 
